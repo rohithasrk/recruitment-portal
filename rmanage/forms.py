0c196a86105b2cdd9ac1e2259efe42e5a038538c
Rohith Asrk <rohith.asrk@gmail.com>
2017-02-05 22:03:36 +0530

    Add modelForms.

---
+++ rmanage/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from .models import *
+
+class CompanyForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = Company
+        fields = ('name', 'email', 'password')
+
+class ApplicantForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = Applicant
+        fields = ('name', 'email', 'password')
+
+class ApplicantDetailForm(forms.ModelForm):
+    class Meta:
+        model = ApplicantDetail
+        fields = ('company', 'phone_no', 'resume', 'role')
+
+class RecruitmentDriveForm(forms.ModelForm):
+    end_date = forms.DateField(widget=forms.DateInput())
+    class Meta:
+        fields = ('name', 'role', 'end_date')
+
+class RoundForm(models.Model):
+    class Meta:
+        fields = ('name', 'role')
+
+class PanelForm(models.Model):
+    class Meta:
+        fields = ('name', 'rdrive', 'round_info')
+
+class CollaboratorForm(models.Model):
+    class Meta:
+        fields = ('panel', 'phone_no')
