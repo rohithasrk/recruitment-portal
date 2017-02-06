9a240f2dbfa42af19fd54de70aae22a473782567
Prafulla <anuragprafulla@gmail.com>
2017-02-06 21:10:20 +0530

    Edited admin.py to include all models

--- rmanage/admin.py
+++ rmanage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from rmanage.models import *
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Applicant)
+admin.site.register(ApplicantDetail)
+admin.site.register(RecruitmentDrive)
+admin.site.register(Round)
+admin.site.register(Panel)
+admin.site.register(Collaborator)
+admin.site.register(Score)
