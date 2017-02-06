d9721df4ede16011c170d68ecd923d1d137967dd
Rohith Asrk <rohith.asrk@gmail.com>
2017-02-06 23:55:47 +0530

    Adds logout feature.

--- rmanage/views.py
+++ rmanage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 
 from .models import *
