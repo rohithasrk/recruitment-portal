388775ecb8227cb6ff378991916ca7abd71d9795
Rohith Asrk <rohith.asrk@gmail.com>
2017-02-06 17:44:21 +0530

    Add fields as default_name in django.db.models.options.

--- rmanage/forms.py
+++ rmanage/forms.py
@@ -1,37 +1,40 @@
 from django import forms
+import django.db.models.options as options
 
-from .models import *
+from rmanage.models import *
+
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('fields',)
 
 class CompanyForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Company
-        fields = ('name', 'email', 'password')
+        fields = ['name', 'email', 'password']
 
 class ApplicantForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Applicant
-        fields = ('name', 'email', 'password')
+        fields = ['name', 'email', 'password']
 
 class ApplicantDetailForm(forms.ModelForm):
     class Meta:
         model = ApplicantDetail
-        fields = ('company', 'phone_no', 'resume', 'role')
+        fields = ['company', 'phone_no', 'resume', 'role']
 
 class RecruitmentDriveForm(forms.ModelForm):
     end_date = forms.DateField(widget=forms.DateInput())
     class Meta:
-        fields = ('name', 'role', 'end_date')
+        fields = ['name', 'role', 'end_date']
 
 class RoundForm(models.Model):
     class Meta:
-        fields = ('name', 'role')
+        fields = ['name', 'role']
 
 class PanelForm(models.Model):
     class Meta:
-        fields = ('name', 'rdrive', 'round_info')
+        fields = ['name', 'rdrive', 'round_info']
 
 class CollaboratorForm(models.Model):
     class Meta:
-        fields = ('panel', 'phone_no')
+        fields = ['panel', 'phone_no']
